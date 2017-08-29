8145523885f8155218a3e562da7f1a42a4d060c2
ainiaa <liuwenyuan521888@163.com>
2017-08-30 01:46:03 +0800

    update

--- src/main/java/com/a91coding/payments/controller/UserController.java
+++ src/main/java/com/a91coding/payments/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.a91coding.payments.controller;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
 import com.a91coding.payments.model.User;
 import com.a91coding.payments.service.IUserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 @RequestMapping("/user")
@@ -16,7 +18,8 @@ public class UserController {
     private IUserService userService;
 
     @RequestMapping("/showUser")
-    public String toIndex(HttpServletRequest request, Model model){
+    public String toIndex(HttpServletRequest request,Model model){
+        System.out.println("ddddddddddddddddddddd");
         int userId = Integer.parseInt(request.getParameter("id"));
         User user = this.userService.getUserById(userId);
         model.addAttribute("user", user);
